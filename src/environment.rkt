a72a2dc27cbc2b26ed62356294e4d2c0011d3b33
Vishesh Yadav <vishesh3y@gmail.com>
2016-04-11 10:32:03 -0400

    Cleanup base environment setup as its no longer needed

--- src/environment.rkt
+++ src/environment.rkt
@@ -8,8 +8,6 @@
 (provide name-in-module
          BASE-ENV)
 
-(define-type-alias RenameMap (HashTable Symbol Symbol))
-
 (: name-in-module (-> Symbol Symbol Symbol))
 (define (name-in-module mod name)
   (match mod
@@ -17,71 +15,5 @@
     ['kernel (string->symbol (~a (jsruntime-kernel-module) "." name))]
     [_ (error "Invalid module name")]))
 
-(: add-import (-> RenameMap String Symbol RenameMap))
-(define (add-import env prefix id)
-  (hash-set env id (string->symbol (~a prefix "." (normalize-symbol id)))))
-
-(: add-imports* (-> RenameMap String (Listof Symbol) RenameMap))
-(define (add-imports* env prefix ids*)
-  (foldl (λ ([id : Symbol] [env* : RenameMap])
-           (add-import env* prefix id))
-         env
-         ids*))
-
-(: add-kernel-imports* (-> RenameMap (Listof Symbol) RenameMap))
-(define (add-kernel-imports* env ids*)
-  (add-imports* env (jsruntime-kernel-module) ids*))
-
-(: add-core-imports* (-> RenameMap (Listof Symbol) RenameMap))
-(define (add-core-imports* env ids*)
-  (add-imports* env (jsruntime-core-module) ids*))
-
-(: CORE-IMPORTS RenameMap)
-(define CORE-IMPORTS
-  ;; TODO: Many of these could be represented using JS operators
-  (let ([build-core-env (λ ([pairs* : (Listof (Pairof Symbol Symbol))])
-                          (map (λ (p*)
-                                 (match-define (cons k v) p*)
-                                 (cons k (string->symbol (~a (jsruntime-core-module) "." v))))
-                               pairs*))])
-    ((inst make-immutable-hash Symbol Symbol)
-     (build-core-env
-      '((* . Number.mul)
-        (+ . Number.add)
-        (- . Number.sub)
-        (/ . Number.div)
-        (< . Number.lt)
-        (> . Number.gt)
-        (<= . Number.lte)
-        (>= . Number.gte)
-        (= . Number.equal))))))
-  
 (: BASE-ENV (HashTable Symbol Symbol))
 (define BASE-ENV (hash))
-#;(define BASE-ENV
-  (add-kernel-imports*
-   CORE-IMPORTS
-   '(zero?
-     car
-     cdr
-     list
-     first
-     rest
-     sub1
-     add1
-     displayln
-     equal?
-     values
-     call-with-values
-     not
-     empty?
-     print-values
-     cons
-     null?
-     empty?
-     list?
-
-     ~a
-     string-append
-     display
-     )))
