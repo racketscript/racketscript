d4bb11b4eafc35f5414d1c87f64aafb45d148039
Vishesh Yadav <vishesh3y@gmail.com>
2016-07-07 21:54:04 -0700

    config: add types annotations

--- src/config.rkt
+++ src/config.rkt
@@ -19,19 +19,32 @@
 (: main-source-file (Parameter (Option Path)))
 (define main-source-file (make-parameter #f))
 
+(: jsruntime-kernel-module (Parameter String))
 (define jsruntime-kernel-module (make-parameter "$rjs_kernel"))
+
+(: jsruntime-core-module (Parameter String))
 (define jsruntime-core-module (make-parameter "$rjs_core"))
 
+(: jsruntime-kernel-module-path (-> Path))
 (define (jsruntime-kernel-module-path)
-  (path->complete-path (build-path (output-directory) "runtime" "kernel.js")))
+  (path->complete-path
+   (build-path (output-directory)
+               "runtime"
+               "kernel.js")))
 
+(: jsruntime-core-module-path (-> Path))
 (define (jsruntime-core-module-path)
-  (path->complete-path (build-path (output-directory) "runtime" "core.js")))
+  (path->complete-path
+   (build-path (output-directory)
+               "runtime"
+               "core.js")))
 
 (: print-to-stdout (Parameter Boolean))
 (define print-to-stdout (make-parameter #f))
 
+(: racket-collects-dir (Parameter Path))
 (define racket-collects-dir (make-parameter
                              (build-path "/usr/share/racket/collects")))
 
-(define FFI-CALL-ID '#%js-ffi)
+(: test-environment? (Parameter Boolean))
+(define test-environment? (make-parameter #f))
