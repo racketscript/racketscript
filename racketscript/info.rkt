0b359d0d74d33634fa102e358ea7d8e886a0f426
Vishesh Yadav <vishesh3y@gmail.com>
2016-12-13 03:18:17 -0500

    Add racketscript parent package

---
+++ racketscript/info.rkt
@@ -0,0 +1,15 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps
+  '("base"
+    "racketscript-compiler"
+    "racketscript-extras"))
+(define build-deps
+  '())
+
+(define implies
+  '("racketscript-compiler"
+    "racketscript-extras"))
+
