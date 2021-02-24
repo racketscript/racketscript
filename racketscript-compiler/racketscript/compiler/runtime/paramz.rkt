25e64fbe5eaecf5e290b2d3a20c7df31a2e74cb6
Sam Tobin-Hochstadt <samth@indiana.edu>
2021-02-24 15:50:49 -0500

    Fix spelling.

--- racketscript-compiler/racketscript/compiler/runtime/paramz.rkt
+++ racketscript-compiler/racketscript/compiler/runtime/paramz.rkt
@@ -8,7 +8,7 @@
 (define+provide parameterization-key #js.Paramz.ParameterizationKey)
 (define+provide break-enabled-key #js.Paramz.BreakEndabledKey)
 (define+provide cache-configuration #js.Paramz.BreakEndabledKey)
-(define+provide reparamerterize #js.Paramz.BreakEndabledKey)
 (define+provide extend-parameterization #js.Paramz.extendParameterization)
 (define+provide exception-handler-key #js.Paramz.ExceptionHandlerKey)
 (define+provide (check-for-break) ($/undefined))
+(define+provide (reparameterize v) v)
