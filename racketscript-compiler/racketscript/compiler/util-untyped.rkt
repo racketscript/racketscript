69b2645bc11f2ed7d0c93c19620d263448cfd4ac
Stephen Chang <stephen.chang@umb.edu>
2021-05-21 16:28:04 -0400

    cleanup prev

--- racketscript-compiler/racketscript/compiler/util-untyped.rkt
+++ racketscript-compiler/racketscript/compiler/util-untyped.rkt
@@ -30,93 +30,11 @@
 ;;         #<path:/home/username/racketscript/racketscript-compiler>)
 ;; else return false.
 (define (links-module? mod-path)
-;  (printf "links-module? mod-path: ~v\n" mod-path)
   (define links-file (find-links-file))
   (for/or ([link-path (links #:file links-file #:root? #t)])
-    ;; (printf "link: ~v\n" link)
-    ;; (define link-name
-    ;;   (~a (let-values ([(base last dir?) (split-path link-path)]) last)))
-    ;; (printf "link-name: ~v\n" link-name)
-    ;; (printf "link-path: ~v\n" link-path)
     (and (subpath? link-path mod-path)
-#;         (printf "link result: ~v\n" 
-                   (list link-name link-path))
          (let-values ([(base link-name dir?) (split-path link-path)])
            (list (~a link-name) link-path)))))
-
-#;(define (links-module? mod-path)
-  (printf "links-module? mod-path: ~v\n" mod-path)
-  (define (match-link? dir link)
-    (printf "match-link?: ~v\n" dir)
-    (printf "match-link?: ~v\n" link)
-    (match link
-      [(list 'root path) #:when (absolute-path? path)
-       ;; Links.rktd may have point to root package which is not at current
-       ;; subdir. Eg. /usr/local/.../links.rktl may point to a package in
-       ;; home directory.
-       (subpath? (~a (simplify-path path))
-                 (~a mod-path))]
-      [(list name path)
-       (printf "match-link? link name: ~v\n" name)
-       (printf "match-link? link path: ~v\n" path)
-       (subpath? (~a (simplify-path (build-path dir path)))
-                 (~a mod-path))]
-      [(list name path re) #f]))
-
-  ;; Path LinkEntry -> (list Symbol Path)
-  ;; Returns (list link-name pkg-root-dir)
-  ;; WHERE: LinkEntry is  an entry in links.rktd file
-  (define (link->result link-fpath link)
-    (printf "link->result: ~a\n" link-fpath)
-    (printf "link->result: ~a\n" link)
-    ;; HACK: If the link path is relative path, then we pick
-    ;; the last component
-    (define (fix-relative-path p)
-      (if (relative-path? p)
-          (~a (let-values ([(base last dir?) (split-path p)]) last))
-          p))
-    (match link
-      [(list 'root path) #:when (absolute-path? path)
-       (list (~a (let-values ([(base last dir?) (split-path path)]) last))
-             (string->path path))]
-      [(list name path)
-       (list (if (symbol? name)
-                 (fix-relative-path path)
-                 name)
-             (simplify-path (build-path (path-only link-fpath)
-                                        path)))]
-      [_ (error 'link->result "unsupported form")]))
-
-  ;; Path -> (list Symbol Path)
-  ;; Iterate through each entry in links.rktd file pointed
-  ;; by link-fpath and find the entry with module mod-path
-  (define (find-link link-fpath)
-    (log-rjs-debug "Processing library collection links at: ~a" link-fpath)
-    (define links-dir (path-only link-fpath))
-    (cond
-      [(file-exists? link-fpath)
-       (call-with-input-file link-fpath
-         (λ (p-links-in)
-           (let loop ([links (read p-links-in)])
-             (printf "links: ~v\n" links)
-             (match links
-               ['() #f]
-               [(cons hd tl) (if (match-link? links-dir hd)
-                                 (link->result link-fpath hd)
-                                 (loop tl))]))))]
-      [else
-       (log-rjs-warning "Library collection link file ~a does not exist!" link-fpath)
-       #f]))
-
-  ;; Iterate through each links.rktd file, to find
-  ;; the out
-  (let loop ([links (current-library-collection-links)])
-    (match links
-      ['() #f]
-      [(cons #f tl) (loop tl)]
-      [(cons hd tl) (or (find-link hd)
-                        (loop tl))])))
-
 
 (define (improper->proper l)
   (match l
