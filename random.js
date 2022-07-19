6b16867a172bc20c007337a6d18337af3d32c4e9
gamburgm <mitch.gamburg@gmail.com>
2022-07-19 17:26:52 -0400

    update bootstrapper

--- random.js
+++ random.js
@@ -22,8 +22,14 @@ Expander.namespace_attach_module(Expander.current_namespace(), KERNEL_IMPORT, de
 Expander.namespace_require(KERNEL_IMPORT, demoNamespace);
 Expander.namespace_require(STX_KERNEL_IMPORT, demoNamespace);
 
-// const stxObj = Expander.namespace_syntax_introduce(Expander.datum__gt_syntax(false, SUM), demoNamespace);
+const expandExpr = (e) => {
+    const stxObj = Expander.namespace_syntax_introduce(Expander.datum__gt_syntax(false, e), demoNamespace);
+    return Expander.expand(stxObj, demoNamespace);
+};
 
-// console.log(Expander.expand(stxObj, demoNamespace)._fields[0].tl);
+const compileExpr = (e) => {
+    expandExpr(e);
+    return Expander.compile(e, demoNamespace);
+};
 
-console.log(Expander.compile(Expander.datum__gt_syntax(false, 5), demoNamespace));
+console.log(compileExpr(5));
