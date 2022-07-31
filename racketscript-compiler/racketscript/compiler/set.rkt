c9ac1e06d8888967e7be555db348115fc77bc24b
Mitch Gamburg <mitch.gamburg@gmail.com>
2022-07-31 07:05:08 -0400

    some more set methods

--- racketscript-compiler/racketscript/compiler/set.rkt
+++ racketscript-compiler/racketscript/compiler/set.rkt
@@ -6,10 +6,18 @@
 
 (struct set-impl (contents) #:transparent)
 
-(provide set set-member?)
+(provide set set-member? set-add list->set)
 
 (define-syntax (set stx)
   (syntax-case stx ()
     [(_ e ...) #'(set-impl (make-immutable-hash (list (cons e #t) ...)))]))
 
 (define (set-member? st v) (hash-has-key? (set-impl-contents st) v))
+
+(define (set-add st v)
+  (set-impl (hash-set (set-impl-contents st) v #t)))
+
+(define (list->set lst)
+  (for/fold ([st (set)])
+            ([elem lst])
+    (set-add st elem)))
