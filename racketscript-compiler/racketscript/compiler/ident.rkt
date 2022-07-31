2f9823e5fba02d7d0c4d99b988cc11217fc0baac
Mitch Gamburg <mitch.gamburg@gmail.com>
2022-07-31 07:21:55 -0400

    types fully removed from ident.rkt

--- racketscript-compiler/racketscript/compiler/ident.rkt
+++ racketscript-compiler/racketscript/compiler/ident.rkt
@@ -2,7 +2,9 @@
 
 (require "config.rkt"
          "match.rkt"
-         ;; suspicious but looks like it doesn't have issues
+         "set.rkt"
+         ;; suspicious but looks like these don't cause issues when flattening
+         (only-in racket/bool false?)
          (only-in racket/string string-prefix?))
 
 (provide fresh-id
@@ -26,30 +28,33 @@
   (cond
     [(false? (should-rename? ss)) ss]
     [(reserved-keyword? s)
-     (~a "r" ss)]
+     (string-append "r" ss)]
     [else
+     (match-define `(,ch-first . ,ch-rest) (string->list ss))
+     (apply string-append
+            (cons (normalize-symbol-atom ch-first #t ignores)
+                  (let loop ([ss ch-rest ])
+                    ;; FIXME this used to be much cleaner with pattern matching
+                    (match ss
+                      [`() null]
+                      [`(,fst ,snd . ,rst)
+                       #:when (and (eq? fst "-")
+                                   (eq? snd ">"))
+                       (cons "_to_" (loop rst))]
+                      [`(,fst . ,rst)
+                       (cons (normalize-symbol-atom fst #f ignores) (loop rst))]))))]))
 
-     (match-define (cons ch-first ch-rest) (string->list ss))
-      (apply string-append
-             (cons (normalize-symbol-atom ch-first #t ignores)
-                   (let loop : (Listof String) ([ss : (Listof Char) ch-rest])
-                        (match ss
-                          [(list) null]
-                          [(list* "-" ">" rst) (cons "_to_" (loop rst))]
-                          [(list* c rst) (cons (normalize-symbol-atom c #f ignores) (loop rst))]))))]))
+;; (module+ test
+;;   (check-equal? (normalize-symbol '7am) "_7am")
+;;   (check-equal? (normalize-symbol 'foobar) "foobar")
+;;   (check-equal? (normalize-symbol '+) "__plus_")
+;;   (check-equal? (normalize-symbol 'hello-world) "hello_world")
+;;   (check-equal? (normalize-symbol 'document.write+print (list "." "+"))
+;;                 "document.write+print"
+;;                 "characters in ignores parameter is not replaced")
+;;   (check-equal? (normalize-symbol 'document.write (list ".")) "document.write"
+;;                 "characters in ignores parameter is not replaced"))
 
-(module+ test
-  (check-equal? (normalize-symbol '7am) "_7am")
-  (check-equal? (normalize-symbol 'foobar) "foobar")
-  (check-equal? (normalize-symbol '+) "__plus_")
-  (check-equal? (normalize-symbol 'hello-world) "hello_world")
-  (check-equal? (normalize-symbol 'document.write+print (list "." "+"))
-                "document.write+print"
-                "characters in ignores parameter is not replaced")
-  (check-equal? (normalize-symbol 'document.write (list ".")) "document.write"
-                "characters in ignores parameter is not replaced"))
-
-(: valid-literal-atom? (-> Char Boolean Boolean))
 (define (valid-literal-atom? ch first?)
   (define patt
     ;;TODO: Not entirely sure about the second case. Check specs again.
@@ -57,20 +62,17 @@
         #px"\\$|_|\\p{L}|\\p{Nl}"
         #px"\\$|_|\\p{L}|\\p{Nl}|\\p{Mn}|\\p{Mc}|\\p{Nd}|\\p{Pc}"))
   (regexp-match-exact? patt (string ch)))
-(module+ test
-  (check-true (valid-literal-atom? #\a #f))
-  (check-true (valid-literal-atom? #\$ #f))
-  (check-true (valid-literal-atom? #\_ #f))
-  (check-true (valid-literal-atom? #\3 #f))
-  (check-false (valid-literal-atom? #\3 #t)))
+;; (module+ test
+;;   (check-true (valid-literal-atom? #\a #f))
+;;   (check-true (valid-literal-atom? #\$ #f))
+;;   (check-true (valid-literal-atom? #\_ #f))
+;;   (check-true (valid-literal-atom? #\3 #f))
+;;   (check-false (valid-literal-atom? #\3 #t)))
 
-(: reserved-keyword? (-> Symbol Boolean))
 (define (reserved-keyword? s)
   (set-member? es6-reserved-keywords s))
 
-(: normalize-symbol-atom (->* (Char Boolean) ((Listof String)) String))
 (define (normalize-symbol-atom ch first? [ignores '()])
-  (: char-map (HashTable String  String))
   (define char-map
     #hash(("-" . "_")
           ("?" . "_p")
@@ -96,32 +98,29 @@
     [(hash-has-key? char-map sch)
      (hash-ref char-map sch)]
     [else "_"]))
-(module+ test
-  (check-true #t))
+;; (module+ test
+;;   (check-true #t))
 
 ;;;----------------------------------------------------------------------------
-(: fresh-id-counter (Parameter Nonnegative-Integer))
 ;; Used when test-environment? is true.
 (define fresh-id-counter (make-parameter 0))
 
-(: fresh-id (-> Symbol Symbol))
 (define fresh-id
   (if (test-environment?)
       gensym
       (λ (id)
         (fresh-id-counter (add1 (fresh-id-counter)))
-        (string->symbol (~a id (fresh-id-counter))))))
-(module+ test
-  (check-equal?
-   (parameterize ([test-environment? #t]
-                  [fresh-id-counter 0])
-     (list (fresh-id 'foo)
-           (fresh-id-counter)))
-   (list 'foo1 1)
-   "fresh-id counter should get incremented"))
+        (string->symbol (format "~a~a" id (fresh-id-counter))))))
+;; (module+ test
+;;   (check-equal?
+;;    (parameterize ([test-environment? #t]
+;;                   [fresh-id-counter 0])
+;;      (list (fresh-id 'foo)
+;;            (fresh-id-counter)))
+;;    (list 'foo1 1)
+;;    "fresh-id counter should get incremented"))
 
 ;;-----------------------------------------------------------------------------
-(: es6-reserved-keywords (Setof Symbol))
 (define es6-reserved-keywords
   (list->set
    '(abstract
