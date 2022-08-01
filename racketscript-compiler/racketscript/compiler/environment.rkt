ab95cf55c90e0d954cd904da31405214557b4c2f
gamburgm <mitch.gamburg@gmail.com>
2022-08-01 16:21:56 -0400

    remove types from environment

--- racketscript-compiler/racketscript/compiler/environment.rkt
+++ racketscript-compiler/racketscript/compiler/environment.rkt
@@ -1,16 +1,14 @@
-#lang typed/racket/base
+#lang racket/base
 
-(require racket/format
-         racket/match
-         "config.rkt")
+(require "config.rkt"
+         "util.rkt")
 
 (provide name-in-module
          *quoted-binding-ident-name*)
 
 (define *quoted-binding-ident-name* '__rjs_quoted__)
 
-(: name-in-module (-> Symbol Symbol Symbol))
 (define (name-in-module mod name)
   (match mod
-    ['core (string->symbol (~a (jsruntime-core-module) "." name))]
-    [_ (error "Invalid module name")]))
+    [`core (string->symbol (~a (jsruntime-core-module) "." name))]
+    [`,_ (error "Invalid module name")]))
