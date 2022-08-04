e817dd7e035968e9e174c464b2671bb133c8faf1
gamburgm <mitch.gamburg@gmail.com>
2022-08-04 11:11:32 -0400

    check patterns at compile time and add guards to struct-match

--- racketscript-compiler/racketscript/compiler/struct-match.rkt
+++ racketscript-compiler/racketscript/compiler/struct-match.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require (for-syntax racket/base))
+(require (for-syntax racket/base racket/struct-info))
 
 (provide struct-match struct-match-define struct-match-lambda)
 
@@ -36,42 +36,73 @@
   (syntax-case pat (?)
     [(? _ id) #'id]))
 
+(define-for-syntax (extract-guard body)
+  (syntax-case body ()
+    [(#:when guard-expr . body)
+     #'guard-expr]
+    [_ #f]))
+
+(define-for-syntax (remove-guard body)
+  (syntax-case body ()
+    [(#:when guard-expr . body)
+     #'body]
+    [_ body]))
+
+(define-for-syntax (validate-struct-patterns! patterns)
+  (cond
+    [(null? patterns) (void)]
+    [else
+     (syntax-case (car patterns) ()
+       [(s . pats)
+        (if (identifier? #'s)
+          (let ([str-info? (syntax-local-value #'s (λ () #f))])
+            (if (not (struct-info? str-info?))
+              (raise (format "struct-match: ~a is not a struct") (syntax->datum #'s))
+              (let ([info (extract-struct-info str-info?)])
+                (displayln (cadddr info))
+                ;; TODO the spec for struct-info has additional details about what can appear
+                ;;      in the struct-info field-accessors position, so it may cause bugs
+                (if (not (eq? (length (cadddr info)) (length (syntax->list #'pats))))
+                  (raise (format "struct-match: struct ~a expects ~a fields, got ~a in pattern"
+                                 #'s
+                                 (length (cadddr info))
+                                 (length (syntax->list #'pats))))
+                  (validate-struct-patterns! (cdr patterns))))))
+          (raise (format "struct-match: unrecognizable head pattern ~a" #'s)))]
+       [_ (validate-struct-patterns! (cdr patterns))])]))
 
 (define-syntax (struct-match stx)
   (syntax-case stx ()
     [(_ expr [pattern body0 body ...] ...)
      #`(let ([v expr])
-         (if (struct? v)
-           #,(let ([patterns (syntax->list #'(pattern ...))])
-               (let loop ([patterns patterns]
-                          [bodys (syntax->list #'((body0 body ...) ...))])
-                 (cond
-                   [(null? patterns)
-                    #'(error 'match "failed ~e" v)]
-                   [(empty-pat? (car patterns)) #`(begin . #,(car bodys))]
-                   [(pred-pat? (car patterns))
-                    #`(if #,(pred-from-pat (car patterns) #'v)
-                        #,(if (empty-pat? (id-of-pred-pat (car patterns)))
-                            #`(let () . #,(car bodys))
-                            #`(let ([#,(id-of-pred-pat (car patterns)) v])
-                                . #,(car bodys)))
-                        #,(loop (cdr patterns) (cdr bodys)))]
-                   [(identifier? (car patterns))
-                    #`(let ([#,(car patterns) v]) . #,(car bodys))]
-                   [else
-                    #`(let* ([vec-v (struct->vector v)]) ;; TODO maybe don't need vector if using pat-pred crap
-                        (cond
-                          [(and (#,(pat-pred (car patterns)) v)
-                                (not (= (vector-length vec-v) #,(pat-length (car patterns)))))
-                           ;; TODO make better, want to blow up if pattern doesn't correspond to real struct
-                           (error 'match "head pattern wrong size")]
-                          [(and (#,(pat-pred (car patterns)) v)
-                                (= (vector-length vec-v) #,(pat-length (car patterns))))
-                           (let-values ([#,(pat-ids (car patterns)) (vector->values vec-v 1)])
-                             . #,(car bodys))]
-                          [else
-                           #,(loop (cdr patterns) (cdr bodys))]))])))
-           (error 'match "~e not a struct" v)))]))
+         #,(let ([patterns (syntax->list #'(pattern ...))])
+             (let loop ([patterns patterns]
+                        [bodys (syntax->list #'((body0 body ...) ...))])
+               (validate-struct-patterns! patterns)
+               (cond
+                 [(null? patterns)
+                  #'(error 'match "failed ~e" v)]
+                 [(empty-pat? (car patterns)) #`(begin . #,(car bodys))]
+                 [(pred-pat? (car patterns))
+                  #`(if #,(pred-from-pat (car patterns) #'v)
+                      #,(if (empty-pat? (id-of-pred-pat (car patterns)))
+                          #`(let () . #,(car bodys))
+                          #`(let ([#,(id-of-pred-pat (car patterns)) v])
+                              . #,(car bodys)))
+                      #,(loop (cdr patterns) (cdr bodys)))]
+                 [(identifier? (car patterns))
+                  #`(let ([#,(car patterns) v]) . #,(car bodys))]
+                 [else
+                  (define guard? (extract-guard (car bodys)))
+                  #`(let* ([vec-v (struct->vector v)]) ;; TODO maybe don't need vector if using pat-pred crap
+                      (define (alt) #,(loop (cdr patterns) (cdr bodys)))
+                      (cond
+                        [(#,(pat-pred (car patterns)) v)
+                         (let-values ([#,(pat-ids (car patterns)) (vector->values vec-v 1)])
+                           #,(if guard?
+                               #`(if #,guard? (begin . #,(car bodys)) (alt))
+                               #`(begin . #,(car bodys))))]
+                        [else (alt)]))]))))]))
 
 (define-syntax (struct-match-define stx)
   (syntax-case stx ()
