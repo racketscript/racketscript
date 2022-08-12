38bc908c37aec0a45c7abf8d9573b034e54d4140
Mitch Gamburg <mitch.gamburg@gmail.com>
2022-08-12 15:30:03 -0400

    ridiculous script to help with compiling runtime

---
+++ flash-runtime.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+rm -rf ~/rs-orig/racketscript-compiler/racketscript/compiler/runtime/
+rm -rf js-build/runtime/
+
+cp -r racketscript-compiler/racketscript/compiler/runtime/ ~/rs-orig/racketscript-compiler/racketscript/compiler/
+
+pushd ~/rs-orig/
+./racketscript-compiler/bin/racks foobar.rkt
+
+cp -r js-build/runtime/ ~/racketscript/js-build/
+
+popd
