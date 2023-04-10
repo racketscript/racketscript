fbe1b961f7152decc8ddbfd544752066aa50f8be
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2023-04-10 16:49:18 +0100

    Update image.rkt
    default color to white not fixed to white

--- racketscript-extras/racketscript/htdp/image.rkt
+++ racketscript-extras/racketscript/htdp/image.rkt
@@ -268,9 +268,9 @@
              (#js.ctx.lineTo (posn-x pt) (posn-y pt))
              (loop (cdr points)))))))])
 
-(define (empty-scene width height)
-  (rectangle width height "solid" "white")
-  (rectangle width height "outline" "black"))   
+(define (empty-scene width height [color "white"])
+  (rectangle width height "solid" color)
+  (rectangle width height "outline" "black"))
 
 (define (text txt size color)
   (new (Text txt
